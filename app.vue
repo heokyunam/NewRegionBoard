bf80ef5906ec11925452ac365123f47e28b9a4a2
hurgyunam <heokyunam@naver.com>
2023-10-03 22:11:17 +0900

    프로젝트 생성 및 tailwind 테스트

---
+++ app.vue
@@ -0,0 +1,221 @@
+<template>
+
+<div class="w-[888px] h-[786px] relative">
+  <div class="w-[888px] h-[760px] left-0 top-[26px] absolute bg-zinc-300"></div>
+  <div class="w-[360px] h-[786px] left-[528px] top-0 absolute">
+    <div class="w-[360px] h-[760px] left-0 top-[26px] absolute bg-neutral-50">
+      <div class="left-[26px] top-[56px] absolute justify-start items-start gap-4 inline-flex">
+        <div class="flex-col justify-start items-center gap-0.5 inline-flex">
+          <div class="text-zinc-800 text-sm font-normal font-['Noto Sans KR']">INVENTORY</div>
+        </div>
+        <div class="flex-col justify-start items-center gap-0.5 inline-flex">
+          <div class="text-zinc-500 text-sm font-normal font-['Noto Sans KR']">CRAFT</div>
+        </div>
+        <div class="flex-col justify-start items-center gap-0.5 inline-flex">
+          <div class="text-zinc-500 text-sm font-bold font-['Noto Sans KR']">EQUIPMENT</div>
+        </div>
+        <div class="flex-col justify-start items-center gap-0.5 inline-flex">
+          <div class="text-zinc-500 text-sm font-bold font-['Noto Sans KR']">ACTION</div>
+        </div>
+      </div>
+      <div class="w-[360px] h-6 p-2.5 left-0 top-[736px] absolute bg-white flex-col justify-center items-center gap-2.5 inline-flex"></div>
+      <div class="w-[328px] h-[53px] px-4 left-[16px] top-[675px] absolute bg-zinc-800 rounded-lg justify-between items-center inline-flex">
+        <div class="text-white text-base font-bold font-['Noto Sans KR']">나무 열매를 섭취하여 3 회복했습니다.</div>
+        <div class="justify-start items-center flex">
+          <div class="w-6 h-6 relative opacity-0"></div>
+        </div>
+      </div>
+      <div class="w-6 h-6 left-[445px] top-[457px] absolute"></div>
+      <div class="w-[306px] h-12 left-[20px] top-[145px] absolute justify-start items-start inline-flex">
+        <div class="w-[220px] h-12 relative bg-slate-50">
+          <div class="left-[20px] top-[16px] absolute justify-start items-center gap-2 inline-flex">
+            <div class="text-gray-600 text-xs font-bold font-['Noto Sans KR'] leading-none">Name</div>
+          </div>
+          <div class="w-[220px] h-px left-0 top-[47px] absolute">
+            <div class="w-[220px] h-px left-0 top-0 absolute bg-gray-100"></div>
+          </div>
+          <div class="w-[220px] h-px left-0 top-0 absolute">
+            <div class="w-[220px] h-px left-0 top-0 absolute bg-gray-100"></div>
+          </div>
+          <div class="w-px h-12 left-[219px] top-0 absolute"></div>
+          <div class="w-px h-12 left-0 top-0 absolute"></div>
+        </div>
+        <div class="w-[86px] h-12 relative bg-slate-50">
+          <div class="left-[20px] top-[16px] absolute justify-start items-center gap-1 inline-flex">
+            <div class="text-gray-600 text-xs font-bold font-['Noto Sans KR'] leading-none">Spend</div>
+          </div>
+          <div class="w-[86px] h-px left-0 top-[47px] absolute">
+            <div class="w-[86px] h-px left-0 top-0 absolute bg-gray-100"></div>
+          </div>
+          <div class="w-[86px] h-px left-0 top-0 absolute">
+            <div class="w-[86px] h-px left-0 top-0 absolute bg-gray-100"></div>
+          </div>
+          <div class="w-px h-12 left-[85px] top-0 absolute"></div>
+          <div class="w-px h-12 left-0 top-0 absolute"></div>
+        </div>
+      </div>
+      <div class="w-[306px] left-[20px] top-[193px] absolute justify-start items-start inline-flex">
+        <div class="w-[220px] h-9 relative">
+          <div class="left-[20px] top-[10px] absolute justify-start items-center gap-2 inline-flex">
+            <div class="text-gray-600 text-xs font-normal font-['Noto Sans KR'] leading-none">나무</div>
+          </div>
+          <div class="w-[220px] h-px left-0 top-[35px] absolute">
+            <div class="w-[220px] h-px left-0 top-0 absolute bg-gray-100"></div>
+          </div>
+          <div class="w-px h-9 left-[219px] top-0 absolute"></div>
+          <div class="w-px h-9 left-0 top-0 absolute"></div>
+        </div>
+        <div class="w-[86px] h-9 relative bg-slate-50">
+          <div class="left-[63px] top-[10px] absolute justify-start items-center gap-1 inline-flex">
+            <div class="text-gray-600 text-xs font-normal font-['Noto Sans KR'] leading-none">5</div>
+          </div>
+          <div class="w-[86px] h-px left-0 top-[35px] absolute">
+            <div class="w-[86px] h-px left-0 top-0 absolute bg-gray-100"></div>
+          </div>
+          <div class="w-px h-9 left-[85px] top-0 absolute"></div>
+        </div>
+      </div>
+      <div class="w-[306px] h-9 left-[20px] top-[229px] absolute justify-start items-start inline-flex">
+        <div class="w-[220px] h-9 relative bg-indigo-600 bg-opacity-5">
+          <div class="left-[20px] top-[10px] absolute justify-start items-center gap-2 inline-flex">
+            <div class="text-gray-600 text-xs font-normal font-['Noto Sans KR'] leading-none">나무 열매</div>
+          </div>
+          <div class="w-[220px] h-px left-0 top-[35px] absolute">
+            <div class="w-[220px] h-px left-0 top-0 absolute bg-gray-100"></div>
+          </div>
+          <div class="w-px h-9 left-[219px] top-0 absolute"></div>
+          <div class="w-px h-9 left-0 top-0 absolute"></div>
+        </div>
+        <div class="w-[86px] h-9 relative bg-indigo-600 bg-opacity-5">
+          <div class="left-[63px] top-[10px] absolute justify-start items-center gap-1 inline-flex">
+            <div class="text-gray-600 text-xs font-normal font-['Noto Sans KR'] leading-none">3</div>
+          </div>
+          <div class="w-[86px] h-px left-0 top-[35px] absolute">
+            <div class="w-[86px] h-px left-0 top-0 absolute bg-gray-100"></div>
+          </div>
+          <div class="w-px h-9 left-[85px] top-0 absolute"></div>
+        </div>
+      </div>
+      <div class="w-[306px] h-9 left-[21px] top-[265px] absolute justify-start items-start inline-flex">
+        <div class="w-[220px] h-9 relative">
+          <div class="left-[20px] top-[10px] absolute justify-start items-center gap-2 inline-flex">
+            <div class="text-gray-600 text-xs font-normal font-['Noto Sans KR'] leading-none">토끼고기</div>
+          </div>
+          <div class="w-[220px] h-px left-0 top-[35px] absolute">
+            <div class="w-[220px] h-px left-0 top-0 absolute bg-gray-100"></div>
+          </div>
+          <div class="w-px h-9 left-[219px] top-0 absolute"></div>
+          <div class="w-px h-9 left-0 top-0 absolute"></div>
+        </div>
+        <div class="w-[86px] h-9 relative bg-slate-50">
+          <div class="left-[63px] top-[10px] absolute justify-start items-center gap-1 inline-flex">
+            <div class="text-gray-600 text-xs font-normal font-['Noto Sans KR'] leading-none">4</div>
+          </div>
+          <div class="w-[86px] h-px left-0 top-[35px] absolute">
+            <div class="w-[86px] h-px left-0 top-0 absolute bg-gray-100"></div>
+          </div>
+          <div class="w-px h-9 left-[85px] top-0 absolute"></div>
+        </div>
+      </div>
+      <div class="w-[306px] h-9 left-[21px] top-[301px] absolute justify-start items-start inline-flex">
+        <div class="w-[220px] h-9 relative bg-slate-50">
+          <div class="left-[20px] top-[10px] absolute justify-start items-center gap-2 inline-flex">
+            <div class="text-gray-600 text-xs font-normal font-['Noto Sans KR'] leading-none">구운토끼고기</div>
+          </div>
+          <div class="w-[220px] h-px left-0 top-[35px] absolute">
+            <div class="w-[220px] h-px left-0 top-0 absolute bg-gray-100"></div>
+          </div>
+          <div class="w-px h-9 left-[219px] top-0 absolute"></div>
+        </div>
+        <div class="w-[86px] h-9 relative bg-slate-50">
+          <div class="left-[63px] top-[10px] absolute justify-start items-center gap-1 inline-flex">
+            <div class="text-gray-600 text-xs font-normal font-['Noto Sans KR'] leading-none">3</div>
+          </div>
+          <div class="w-[86px] h-px left-0 top-[35px] absolute">
+            <div class="w-[86px] h-px left-0 top-0 absolute bg-gray-100"></div>
+          </div>
+          <div class="w-px h-9 left-[85px] top-0 absolute"></div>
+        </div>
+      </div>
+      <div class="pl-3 pr-2.5 py-[5px] left-[22px] top-[93px] absolute bg-violet-100 rounded-[28px] justify-center items-center gap-1 inline-flex">
+        <div class="text-gray-800 text-xs font-medium font-['Noto Sans KR'] leading-[17.40px]">음식</div>
+        <div class="w-5 h-5 pl-[4.19px] pr-[4.83px] pt-[4.19px] pb-[4.83px] justify-center items-center flex">
+          <img class="w-[10.98px] h-[10.98px]" src="https://via.placeholder.com/11x11" />
+        </div>
+      </div>
+    </div>
+    <div class="w-6 h-6 left-[136px] top-0 absolute"></div>
+  </div>
+  <div class="w-[360px] h-[98px] left-[101px] top-[503px] absolute">
+    <div class="w-[360px] h-[98px] px-6 left-0 top-0 absolute justify-between items-center inline-flex">
+      <div class="justify-start items-center gap-3 flex">
+        <div class="w-10 h-10 relative">
+          <div class="w-10 h-10 left-0 top-0 absolute bg-white rounded-full border border-zinc-800"></div>
+          <div class="w-10 h-10 left-0 top-0 absolute">
+            <div class="w-10 h-10 left-0 top-0 absolute bg-zinc-300 rounded-full"></div>
+            <div class="w-[31.89px] h-[37.49px] left-[3.21px] top-[6.94px] absolute">
+            </div>
+          </div>
+        </div>
+        <div class="flex-col justify-start items-start gap-0.5 inline-flex">
+          <div class="text-zinc-500 text-sm font-bold font-['Noto Sans KR']">Welcome</div>
+          <div class="text-zinc-800 text-lg font-bold font-['Noto Sans KR']">John Doe</div>
+        </div>
+      </div>
+      <div class="w-6 h-6 relative"></div>
+    </div>
+    <div class="w-[360px] h-[98px] px-6 left-0 top-0 absolute justify-between items-center inline-flex">
+      <div class="justify-start items-center gap-3 flex">
+        <div class="w-10 h-10 relative">
+          <div class="w-10 h-10 left-0 top-0 absolute bg-white rounded-full border border-zinc-800"></div>
+          <div class="w-10 h-10 left-0 top-0 absolute">
+            <div class="w-10 h-10 left-0 top-0 absolute bg-zinc-300 rounded-full"></div>
+            <div class="w-[31.89px] h-[37.49px] left-[3.21px] top-[6.94px] absolute">
+            </div>
+          </div>
+        </div>
+        <div class="flex-col justify-start items-start gap-0.5 inline-flex">
+          <div class="text-zinc-500 text-sm font-bold font-['Noto Sans KR']">Welcome</div>
+          <div class="text-zinc-800 text-lg font-bold font-['Noto Sans KR']">John Doe</div>
+        </div>
+      </div>
+      <div class="w-6 h-6 relative"></div>
+    </div>
+    <div class="left-[190px] top-[32px] absolute text-zinc-800 text-sm font-['Noto Sans KR']">24/100</div>
+    <div class="left-[197px] top-[57px] absolute text-zinc-800 text-sm font-['Noto Sans KR']">24/100</div>
+    <div class="left-[273px] top-[32px] absolute text-zinc-800 text-sm font-['Noto Sans KR']">24/100</div>
+    <div class="left-[271px] top-[62px] absolute text-zinc-800 text-sm font-['Noto Sans KR']">24/100</div>
+    <div class="w-6 h-6 left-[165px] top-[29px] absolute"></div>
+    <div class="w-6 h-6 left-[246px] top-[29px] absolute"></div>
+  </div>
+  <div class="w-[450px] h-[425px] left-[29px] top-[44px] absolute">
+    <div class="w-[450px] h-[425px] left-0 top-0 absolute">
+      <img class="w-[90px] h-[85px] left-0 top-[255px] absolute shadow border-4 border-black border-opacity-50" src="https://via.placeholder.com/90x85" />
+      <img class="w-[90px] h-[85px] left-0 top-[340px] absolute shadow border-4 border-black border-opacity-50" src="https://via.placeholder.com/90x85" />
+      <img class="w-[90px] h-[85px] left-[90px] top-[340px] absolute shadow border-4 border-black border-opacity-50" src="https://via.placeholder.com/90x85" />
+      <img class="w-[90px] h-[85px] left-[360px] top-[255px] absolute shadow border-4 border-black border-opacity-50" src="https://via.placeholder.com/90x85" />
+      <img class="w-[90px] h-[85px] left-[270px] top-[340px] absolute shadow border-4 border-black border-opacity-50" src="https://via.placeholder.com/90x85" />
+      <img class="w-[90px] h-[85px] left-[360px] top-[340px] absolute shadow border-4 border-black border-opacity-50" src="https://via.placeholder.com/90x85" />
+      <img class="w-[90px] h-[85px] left-0 top-0 absolute shadow border-4 border-black border-opacity-50" src="https://via.placeholder.com/90x85" />
+      <img class="w-[90px] h-[85px] left-0 top-[85px] absolute shadow border-4 border-black border-opacity-50" src="https://via.placeholder.com/90x85" />
+      <img class="w-[90px] h-[85px] left-0 top-[170px] absolute shadow border-4 border-black border-opacity-50" src="https://via.placeholder.com/90x85" />
+      <img class="w-[90px] h-[85px] left-[360px] top-0 absolute shadow border-4 border-black border-opacity-50" src="https://via.placeholder.com/90x85" />
+      <img class="w-[90px] h-[85px] left-[360px] top-[170px] absolute shadow border-4 border-black border-opacity-50" src="https://via.placeholder.com/90x85" />
+      <img class="w-[90px] h-[85px] left-[360px] top-[85px] absolute shadow border-4 border-black border-opacity-50" src="https://via.placeholder.com/90x85" />
+      <img class="w-[90px] h-[85px] left-[180px] top-[340px] absolute shadow border-4 border-black border-opacity-50" src="https://via.placeholder.com/90x85" />
+      <img class="w-[90px] h-[85px] left-[180px] top-0 absolute shadow border-4 border-black border-opacity-50" src="https://via.placeholder.com/90x85" />
+      <img class="w-[90px] h-[85px] left-[90px] top-0 absolute shadow border-4 border-black border-opacity-50" src="https://via.placeholder.com/90x85" />
+      <img class="w-[90px] h-[85px] left-[270px] top-0 absolute shadow border-4 border-black border-opacity-50" src="https://via.placeholder.com/90x85" />
+    </div>
+    <div class="w-[73px] h-[63px] left-[8px] top-[95px] absolute">
+      <div class="w-[73px] h-[63px] left-0 top-0 absolute bg-white rounded-full border border-zinc-800"></div>
+      <div class="w-[73px] h-[63px] left-0 top-0 absolute">
+        <div class="w-[73px] h-[63px] left-0 top-0 absolute bg-zinc-300 rounded-full"></div>
+        <div class="w-[58.20px] h-[59.05px] left-[5.87px] top-[10.93px] absolute">
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+</template>
